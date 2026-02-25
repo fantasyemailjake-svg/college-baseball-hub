fba61b0fc0d3931c3554cf0ec65e83feae0a46d7
Jake Lecuyer <fantasyemailjake@gmail.com>
2026-02-25 15:44:01 -0600

    Connect JS team list

---
+++ data.js
@@ -0,0 +1,17 @@
+const teams = [
+  {
+    name: "LSU",
+    conference: "SEC",
+    championships: 7
+  },
+  {
+    name: "Texas",
+    conference: "Big 12",
+    championships: 6
+  },
+  {
+    name: "Vanderbilt",
+    conference: "SEC",
+    championships: 2
+  }
+];
